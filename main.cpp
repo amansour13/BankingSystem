a44bdbb978ec18b28c4f8a7adabfc6aa8e5c1bb1
AhmadOsama9 <102378484+AhmadOsama9@users.noreply.github.com>
2022-11-05 21:49:45 +0200

    this is the first part of BigReal

--- main.cpp
+++ main.cpp
@@ -1,58 +1,61 @@
-//#include "BankApplication.h"
-//#include "BankAccount.h"
-//#include "Client.h"
-//#include "SavingsBankAccount.h"
-#include <iostream>
-#include <string>
-#include <map>
-#include <regex>
-#include "BankApplication.h"
+#include<iostream>
+#include"BigDecimalIntClass.h"
 
-using namespace std;
+class BigReal{
 
+    double realNumber;
+    std::string realNum;
+    BigDecimalInt BD;
 
-int main()
+public:
+
+    BigReal(double x = 0.0){ realNumber = x;}
+    BigReal(std::string num){ realNum = num;} 
+    BigReal(BigDecimalInt big) { BD = big;}
+    BigReal(BigReal&);
+    BigReal(BigReal&&);
+    BigReal& operator=(BigReal&);
+    BigReal& operator=(BigReal&&);
+};
+
+BigReal::BigReal(BigReal& other)
 {
-    BankApplication test;
-    for(; ; ) 
-    {  
-        std::string choice = "";
+    realNumber = other.realNumber;
+    realNum = other.realNum;
+    BD = other.BD;
 
-        std::cout << "Welcome to FCAI Banking System\n";
-        std::cout << "1. Create a New Account\n";
-        std::cout << "2. list of Clients and Accounts\n";
-        std::cout << "3. Withdraw money\n";
-        std::cout << "4. Deposit money\n";
-        std::cout << "0. Exit\n";
+}
 
-        std::cin >> choice;
+BigReal::BigReal(BigReal&& other)
+{
+    realNumber = other.realNumber;
+    realNum = other.realNum;
+    BD = other.BD;
 
-        if(choice == "1")
-            test.add_new_account();
-        else if(choice == "2")
-            test.printList();
-        else if(choice == "3")
-        {
-            double money;
-            std::cout << "Enter the money that you want to withdraw\n"; std::cin >> money;
-            /*
-            I'm trying to access the last object cause there's nothing like a login so the last one will be the one that we currenly working at.
-            */
-            BankAccount acc = test.listOfClients[test.listOfClients.size() - 1];
-            acc.withdraw(money);
-        }
-        else if(choice == "4")
-        {
-            double money;
-            std::cout << "Enter the money that you want to withdraw\n"; std::cin >> money;
+}
 
-            BankAccount acc = test.listOfClients[test.listOfClients.size() - 1];
-            acc.withdraw(money);
-        }
-        else if(choice == "0")
-            break;
-        else
-            std::cout << "Enter a valid input \n";
+BigReal& BigReal::operator=(BigReal& other)
+{
+    realNumber = other.realNumber;
+    realNum = other.realNum;
+    BD = other.BD;
+}
 
+BigReal& BigReal::operator=(BigReal&& other)
+{
+    if(this != &other)
+    {
+        realNumber = other.realNumber;
+        realNum = other.realNum;
+        BD = other.BD;
     }
+}
+
+
+
+int main(){
+
+
+
+
 }
